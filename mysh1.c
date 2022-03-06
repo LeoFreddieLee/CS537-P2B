385d74a6b4606adb5397746ce95c67ac96bceaf9
unknown <wxu7244@wisc.edu>
2022-03-06 02:05:23 -0600

    new

--- mysh1.c
+++ mysh1.c
@@ -117,7 +117,7 @@ int main(int argc, char *argv[]) {
 	fclose(file);
 	if(flag){
 	write(1, "mysh> ", strlen("mysh> "));
-	          continue;
+	    continue;
 	}
 	
         //fork()
@@ -192,7 +192,36 @@ int main(int argc, char *argv[]) {
             }
 	      //printf("mysh> ");
 	      
-      }else{
+      }else if(strcmp(myargv[0], "unalias") == 0){
+         if(myargc == 3) {
+             int point = 0;
+             for(int i = 0; i < ac; i++) {
+                  if(strcmp(alias[i][0], myargv[1]) == 0) {
+		    printf("%s", alias[i][0]);
+		    printf("%s", myargv[1]);
+		    printf("%d", i);
+		    printf("%d", ac);
+                    point = i;
+                   }	
+	     }
+	     if(ac==1){
+		free(alias[0][0]);
+		free(alias[0][1]);
+		alias[0][0] = (char*)malloc(512*sizeof(char));
+		alias[0][1] = (char*)malloc(512*sizeof(char));
+
+	     }else{
+		for (i = point; i < ac -1; i++)  {
+			alias[i][0] = alias[i+1][0];
+			alias[i][1] = alias[i+1][1];
+								               }
+	     }
+	     
+	 }else{
+	    printf("%s", "unalias: Incorrect number of arguments.\n");
+	 }
+      }
+      else{
 	      for(int i = 0; i < ac; i++){
            if(strcmp(myargv[0], alias[i][0]) == 0){
              strcpy(myargv[0], alias[i][1]);
@@ -234,6 +263,120 @@ int main(int argc, char *argv[]) {
       if(strlen(input)==1){continue;}
       input[strlen(input)-1] = 0;
       const char s[2] = " "; // deliminater
+      const char direction[2] = ">"; //redirection delimater
+      bool judge = false;
+      int count =0;
+      for(int i = 0; i < strlen(input); i++){
+        if(input[i] == direction[0]) {
+	        count++;
+          judge = true;
+        }
+      }
+      if(input[0] == direction[0] || count >1) {
+	      printf("%s \n", "Redirection misformatted.");
+	      write(1, "mysh> ", strlen("mysh> "));
+	      continue;
+      }
+      
+      if (judge){
+        char *dup0 = strdup(input);
+        char *dup1 = strdup(input);
+        //get whole direction arg
+        int tmp = 0;
+        char *sequence = strtok(dup0, direction);
+        while(sequence != NULL){
+          tmp++;
+          sequence = strtok(NULL, direction);
+        }
+        tmp++;
+        char *tmpArg[tmp];
+        char *arg = NULL;
+        int i = 0;
+        arg = strtok(dup1, direction);
+        while(arg != NULL){
+          tmpArg[i] = arg;
+          i++;
+          arg = strtok(NULL, direction);
+        }
+        tmpArg[i]=NULL;
+        //get the command's argv
+        tmp=0;
+	char *d1 = strdup(tmpArg[0]);
+	char *d2 = strdup(tmpArg[0]);
+        sequence=strtok(d1,s );
+        while(sequence != NULL){
+          tmp++;
+          sequence = strtok(NULL, s);
+        }
+        tmp++;
+        char *comArg[tmp];
+        i=0;
+        arg = strtok(d2, s);
+        while(arg != NULL){
+          comArg[i] = arg;
+          i++;
+          arg = strtok(NULL, s);
+        }
+        comArg[i] = NULL; 
+	//get file arg
+	tmp=0;
+	char *d3 = strdup(tmpArg[1]);
+	char *d4 = strdup(tmpArg[1]);
+	sequence=strtok(d3,s);
+  	while(sequence != NULL){
+          tmp++;
+          sequence = strtok(NULL,s);
+	}
+        tmp++;
+        char *fileArg[tmp];
+        i=0;
+        arg = strtok(d4, s);
+        while(arg != NULL){
+          fileArg[i] = arg;
+          i++;
+          arg = strtok(NULL, s);
+           }
+        fileArg[i]=NULL;
+	if(i>1||i==0){
+	  printf("%s \n", "Redirection misformatted.");
+	  write(1, "mysh> ", strlen("mysh> "));
+          continue;
+	}
+	bool flag = false;
+	FILE *file = fopen(fileArg[0], "w");
+	if(file == NULL){
+	  printf("Cannot write to file %s.\n",fileArg[0]);
+	  flag=true;
+	}
+	fclose(file);
+	if(flag){
+	write(1, "mysh> ", strlen("mysh> "));
+	    continue;
+	}
+	
+        //fork()
+        int c = fork(); 
+             if (c < 0) {
+	             write(1, "fork failed", strlen("fork failed"));
+	      } else if (c == 0){ 
+		      fclose(stdout);
+		      FILE *fp = fopen(fileArg[0], "w");
+		      
+	             if(execv(comArg[0], comArg) == -1) {
+	                     printf("%s: Command not found.\n", comArg[0]);
+		      fflush(stdout);
+               _exit(1);  
+            }
+		fclose(fp);
+		
+                
+      } else{
+          int c_wait = waitpid(c, NULL, 0);
+          printf("mysh> child process %d (parent: %d) is terminated. (%d)\nmysh> ", c_wait, (int) getpid(), c);
+	  fflush(stdout);
+      }	
+	continue;  
+      }
       char *dup = strdup(input); // dup is for argument counting
       char *dup2 = strdup(input); // dup2 is for argument values
       char *num = strtok(dup, s); // argument counting
